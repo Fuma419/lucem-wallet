d14761547a4ccc0aad2d621c7e29e447d207d675
Benjamin <31835324+Fuma419@users.noreply.github.com>
2024-06-08 18:34:42 -0400

    fixing logos. adding 'focus-visible' dep.

--- src/ui/theme.jsx
+++ src/ui/theme.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { POPUP_WINDOW } from '../config/config';
 import './app/components/styles.css';
 import '@fontsource/ubuntu/latin.css';
+import 'focus-visible/dist/focus-visible';
 
 const colorMode = localStorage['chakra-ui-color-mode'];
 
@@ -26,19 +28,19 @@ const Input = {
     },
   },
   defaultProps: {
-    focusBorderColor: 'teal.500',
+    focusBorderColor: 'yellow.500',
   },
 };
 
 const Checkbox = {
   defaultProps: {
-    colorScheme: 'teal',
+    colorScheme: 'yellow',
   },
 };
 
 const Select = {
   defaultProps: {
-    focusBorderColor: 'teal.500',
+    focusBorderColor: 'yellow.500',
   },
 };
 
@@ -57,11 +59,58 @@ const Switch = {
     },
   },
   defaultProps: {
-    colorScheme: 'teal',
+    colorScheme: 'yellow',
   },
 };
 
 const theme = extendTheme({
+  colors: {
+    yellow: { 
+      100: "#E1EDAD",
+      200: "#DBF07F",
+      300: "#D8F261",
+      400: "#D2F538",
+      500: "#CCFA00",
+      600: "#B1D900",
+      700: "#94B500",
+      800: "#758F00",
+      900: "#4F6100",
+    },
+    teal: { 
+      100: "#E1EDAD",
+      200: "#DBF07F",
+      300: "#D8F261",
+      400: "#D2F538",
+      500: "#CCFA00",
+      600: "#B1D900",
+      700: "#94B500",
+      800: "#758F00",
+      900: "#4F6100",
+    },
+    gray: { 
+      100: "#CCCCCC",
+      200: "#BABAB8",
+      300: "#B3B3B3",
+      400: "#A1A1A1",
+      500: "#8C8C8B",
+      600: "#787877",
+      700: "#616160",
+      800: "#4D4D4D",
+      900: "#383838",
+    },
+    orange: { 
+      100: "#E8AA00",
+      200: "#DBA100",
+      300: "#D19900",
+      400: "#C49000",
+      500: "#B08102",
+      600: "#997000",
+      700: "#856100",
+      800: "#6B4F00",
+      900: "#6B4F00",
+    },
+  },
+
   components: {
     Checkbox,
     Input,
